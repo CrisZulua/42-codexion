4063a8f154b9ecfad6ff4b220fcc62a6703b6e0f
CrisZulua <cristianzulu98@gmail.com>
2026-07-24 16:02:35 +0200

    PARSER: arguments check

---
+++ coders/parse_args.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: czuluaga <czuluaga@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/24 14:53:09 by czuluaga          #+#    #+#             */
+/*   Updated: 2026/07/24 16:02:10 by czuluaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include "parse_args.h"
+
+int is_positive(char *number)
+{
+	int i;
+
+	i = 0;
+	if (number[i] == '-')
+		return (0);
+	if (number[i] == '+')
+		i++;
+	while (i < (int)strlen(number))
+	{
+		if (number[i] < '0' || number[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int parse_args(int argc, char **argv)
+{
+	int i;
+
+	if (argc < 9)
+		return (printf("%s\n", NOT_ENOUGH_ARGS), 0);
+	i = 1;
+	while (i < 8)
+	{
+		if (!is_positive(argv[i]))
+			return (printf("%s\n", BAD_NUMBER_VALUE), 0);
+		i++;
+	}
+	if (strcmp(argv[i], "fifo") != 0 && strcmp(argv[i], "edf") != 0)
+		return (printf("%s\n", UNKNOWN_SCHEDULER), 0);
+	return (1);
+}
---
+++ coders/parse_args.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_args.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: czuluaga <czuluaga@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/24 14:59:16 by czuluaga          #+#    #+#             */
+/*   Updated: 2026/07/24 15:51:09 by czuluaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSE_ARGS_H
+#define PARSE_ARGS_H
+
+#define NOT_ENOUGH_ARGS "usage: codexion n_coders time_to_burnout "      \
+						"time_to_compile time_to_debug time_to_refactor" \
+						" n_compiles_required dongle_cooldown "          \
+						"scheduler(fifo|edf)"
+#define BAD_NUMBER_VALUE "Argument type has to be a positive integer"
+#define UNKNOWN_SCHEDULER "Scheduler: <fifo | edf>"
+
+/*
+	DESCRIPTION
+	Checks wether the string given as argument represents a positive number.
+	Only admits '+' at the beginning of the string.
+
+	RETURN VALUE
+	1 if falls into the category described.
+	0 otherwise.
+*/
+int is_positive(char *number);
+
+/*
+	DESCRIPTION
+	Checks correct format on every argument give.
+
+	Positive numbers, ARGS 0-7:
+		number_of_coders time_to_burnout time_to_compile time_to_debug
+		time_to_refactor number_of_compiles_required dongle_cooldown
+
+	String ("fifo" | "edf") ARG 8:
+		scheduler
+
+	If there is an error, a custom message gets printed.
+
+	RETURN VALUE
+	1 if everything is ok with the arguments
+	0 if there is an error on any argument
+*/
+int parse_args(int argc, char **argv);
+
+#endif
